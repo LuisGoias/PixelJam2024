de7076e579e8ad3015da9e042952f443b60b96da
hiba-alimam <101088146+hiba-alimam@users.noreply.github.com>
2024-05-19 18:06:40 +0300

    BGM files added

---
+++ Assets/BGMSwitch.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMSwitch : MonoBehaviour
+{
+    [SerializeField] AudioClip MainMenu;
+    [SerializeField] AudioClip InGame;
+    [SerializeField] AudioSource audioSource;
+
+    AudioClip current;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchSong()
+    {
+        if (true)
+        {
+            
+        }
+    }
+
+}
